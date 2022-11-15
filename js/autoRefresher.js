049b13d8aff31c2177f7a42132d917f7f2223bef
olivierdalang <olivier.dalang@gmail.com>
2022-11-15 17:06:59 +0100

    include auto-refresher

--- js/autoRefresher.js
+++ js/autoRefresher.js
@@ -1,18 +1,31 @@
 
 import StandardStore from '../qwc2/stores/StandardStore';
+import { changeLayerProperty } from '../qwc2/actions/layers';
 
 
-
+let refreshInc = 0;
 function refreshLayer() {
     console.log("Refreshing layers...");
 
     // Get the redux store
     let store = StandardStore.get();
-    console.log("store is "+store);
+
+    // Dispatch an action that refreshes
+    let state = store.getState();
+    state.layers.flat.forEach(layer => {
+        // TODO: url needs to take into account host and scheme
+        if( layer.url && layer.url.startsWith("http://localhost/webgis/qgis_server_proxy/ogc/") )
+        {
+            store.dispatch(
+                changeLayerProperty(
+                    layer.uuid, "url", layer.url + `?refresh=${refreshInc}`
+                )
+            );
+        }
+    });
+
+    refreshInc += 0.1;
 }
 
 console.log("Installing auto-refresher...");
 setInterval(refreshLayer, 5000);
-
-
-
